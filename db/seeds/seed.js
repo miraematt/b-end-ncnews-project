d70a72ea1a9bb9159a7a2da0ed6d6f4b186230fe
miraematt <matttootell@hotmail.com>
2019-06-04 08:23:45 +0100

    complete seeding topics users articles

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,10 +1,55 @@
-// const {  } = require('../data');
+const {
+  articlesData,
+  commentsData,
+  topicsData,
+  usersData
+} = require("../data/index");
+const { formatArticles } = require("../../utils/index");
+// const { ownerData, shopData, treasureData } = require("../data/index");
+// const {
+//   createOwnerRef,
+//   createShopRef,
+//   formatShops,
+//   formatTreasures
+// } = require("../../utils/index");
+
+// js
+//  db/seeds/index.js
+// const { houseData, wizardData } = require('../data');
+
+// exports.seed = function(knex, Promise) {
+//   return knex.migrate
+//     .rollback()
+//     .then(() => knex.migrate.latest())
+//     .then(() => {
+//       return knex('houses')
+//         .insert(houseData)
+//         .returning('*');
+//     })
+//     .then((houseRows) => {
+//       /<-- do the rest of the seed logic here ...
+//     });
+// };
 
 exports.seed = (knex, Promise) => {
   return knex.migrate
     .rollback()
     .then(() => knex.migrate.latest())
     .then(() => {
-      // insert data
+      return knex("topics")
+        .insert(topicsData)
+        .returning("*");
+    })
+    .then(() => {
+      return knex("users")
+        .insert(usersData)
+        .returning("*");
+    })
+    .then(() => {
+      const formattedArticles = formatArticles(articlesData);
+      console.log(formattedArticles);
+      return knex("articles")
+        .insert(formattedArticles)
+        .returning("*");
     });
 };
