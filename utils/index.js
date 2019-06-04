7c7cacabada8e92376df062b771d1c6851a9b598
miraematt <matttootell@hotmail.com>
2019-06-04 10:15:54 +0100

    complete articles seeding

--- utils/index.js
+++ utils/index.js
@@ -8,10 +8,8 @@ exports.formatArticles = articles => {
     formattedArticles[i] = {};
     for (key in articles[i]) {
       formattedArticles[i][key] = articles[i][key];
-      // formattedArticles[i]["article_id"] = i + 1;
-      // formattedArticles[i]["votes"] = 0;
       let dateToFormat = new Date(articles[i]["created_at"]);
-      let dateString = dateToFormat.toISOString().slice(0, 10);
+      let dateString = dateToFormat.toISOString();
       // let dateString = moment(dateToFormat).format("YYYY-MM-DD");
       articles[i]["created_at"] = dateString;
     }
