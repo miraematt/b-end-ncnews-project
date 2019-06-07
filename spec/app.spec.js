e31b7b099b5084154a8f567a49a7f518ff008f69
miraematt <matttootell@hotmail.com>
2019-06-07 15:25:29 +0100

    delete complete just testing to check

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -548,5 +548,47 @@ describe.only("/", () => {
           });
       });
     });
+    describe("/comments/:comment_id", () => {
+      it("DELETE status:204 it deletes the requested house", () => {
+        return request(app)
+          .delete("/api/comments/1")
+          .expect(204);
+      });
+
+      it("POST status:405 Method Not Allowed for all methods that we cannot use", () => {
+        return request(app)
+          .post("/api/comments/1")
+          .expect(405)
+          .then(({ body }) => {
+            expect(body.msg).to.equal("Method Not Allowed");
+          });
+      });
+      // it("DELETE status:404 Not Found for comment_id that does not exist", () => {
+      //   return request(app)
+      //     .delete("/api/comments/1234567890")
+      //     .expect(404)
+      //     .then(({ body }) => {
+      //       expect(body.msg).to.equal("No comment found for this comment id");
+      //     });
+      // });
+      // it("PATCH status:400 Bad Request for invalid increment", () => {
+      //   return request(app)
+      //     .patch("/api/comments/1")
+      //     .send({ inc_votes: "yes" })
+      //     .expect(400)
+      //     .then(({ body }) => {
+      //       expect(body.msg).to.equal("Bad Request");
+      //     });
+      // });
+      // it("PATCH status:400 Bad Request for article_id that is not a positive integer", () => {
+      //   return request(app)
+      //     .patch("/api/articles/hello")
+      //     .send({ inc_votes: 1 })
+      //     .expect(400)
+      //     .then(({ body }) => {
+      //       expect(body.msg).to.equal("Bad Request");
+      //     });
+      // });
+    });
   });
 });
